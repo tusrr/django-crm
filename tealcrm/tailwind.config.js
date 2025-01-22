9c7159e3f59aad48fe298d1e5c96ddf43daa403b
tusrr <tushar.raj2000@gmail.com>
2025-01-22 09:16:07 +0530

    Add Tailwind CSS integration and update static file settings

---
+++ tealcrm/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "client/templates/clients/*.html",
+    "core/templates/core/*.html",
+    "dashboard/templates/dashboard/*.html",
+    "leads/templates/leads/*.html",
+    "team/templates/team/*.html",
+    "userprofile/templates/userprofile/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
